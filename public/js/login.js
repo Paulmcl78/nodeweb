d54de7a1d3c956fbb3cca79b136548c9fb517bcd
Paul McLaughlin <pmclaughlin@navinet.net>
2016-02-24 15:12:40 +0000

    Adding templates

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 
-function AppViewModel() {
+function MenuViewModel() {
     this.authenticated = ko.observable(false);
     this.username = ko.observable('username');
     this.email = ko.observable('');
@@ -11,17 +11,24 @@ function AppViewModel() {
     this.signOut = function()
     {
         this.authenticated(false);
-    }
-    
-    this.dosomething=function(){
-        var dialogEl = document.getElementById('content');
-        ko.applyBindings(testviewmodel, dialogEl);
-    }
+    } 
 }
 
-function testviewmodel() {
+function aboutviewmodel() {
     name="test";
 }
+
+
+// Top binding
 var topNavBar = document.getElementById('topNavBar');
-// Activates knockout.js
-ko.applyBindings(new AppViewModel(),topNavBar);
+ko.applyBindings(new MenuViewModel(),topNavBar);
+
+//Home page binding
+this.aboutvisible = ko.obervable(false);
+this.showabout = function()
+{
+        var dialogEl = document.getElementById('aboutcontent');
+        ko.applyBindings(aboutviewmodel, dialogEl);
+        this.aboutvisible(true);
+        
+}
